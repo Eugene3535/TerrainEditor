bc9b36c19f12ff4b5f406d574833c341d7baec81
Eugene <63926730+Eugene3535@users.noreply.github.com>
2022-08-21 22:06:35 +0300

    Delete build directory

--- build/external/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/OpenGl/TerrainEditor/external/glm/glm
-# Build directory: D:/Projects/OpenGl/TerrainEditor/build/external/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
